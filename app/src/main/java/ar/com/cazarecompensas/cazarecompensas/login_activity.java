52ee2c47bc81bbe5d22dc3316740075520e3de4c
Julian Matias Bermolen <jbermolen@dlatv.net>
2017-09-01 16:14:55 -0300

    Segundo commit

--- app/src/main/java/ar/com/cazarecompensas/cazarecompensas/login_activity.java
+++ app/src/main/java/ar/com/cazarecompensas/cazarecompensas/login_activity.java
@@ -26,15 +26,6 @@ public class login_activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         callbackManager = CallbackManager.Factory.create();
 
         loginButton = (LoginButton) findViewById(R.id.loginButton);
