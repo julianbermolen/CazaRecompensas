dfa2c62acddb328c92a15edc465f27aeaccf3e23
lauuteeh <lauuteeh@users.noreply.github.com>
2017-10-27 20:15:40 -0300

    Mapa | Se obtiene la ubicacion actual

--- app/src/main/java/ar/com/cazarecompensas/cazarecompensas/MapaTesoros.java
+++ app/src/main/java/ar/com/cazarecompensas/cazarecompensas/MapaTesoros.java
@@ -1,22 +1,37 @@
 package ar.com.cazarecompensas.cazarecompensas;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import static ar.com.cazarecompensas.cazarecompensas.R.id.map;
 
 
 /**
@@ -24,13 +39,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Activities that contain this fragment must implement the
  * {@link MapaTesoros.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MapaTesoros#newInstance} factory method to
+ * Use the {@link MapaTesoros} factory method to
  * create an instance of this fragment.
  */
 public class MapaTesoros extends Fragment implements OnMapReadyCallback {
-    GoogleMap mGoogleMap;
+    private GoogleMap mMap;
     MapView mMapView;
     View mView;
+    private Marker marcador;
+    private Circle circle;
+    double lat = 0.0;
+    double lng = 0.0;
 
     public MapaTesoros() {
         // Required empty public constructor
@@ -45,7 +64,7 @@ public class MapaTesoros extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mView= inflater.inflate(R.layout.fragment_mapa_tesoros, container, false);
+        mView = inflater.inflate(R.layout.fragment_mapa_tesoros, container, false);
         return mView;
     }
 
@@ -53,9 +72,9 @@ public class MapaTesoros extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mMapView = (MapView)mView.findViewById(R.id.map);
+        mMapView = (MapView) mView.findViewById(map);
 
-        if(mMapView != null){
+        if (mMapView != null) {
 
             mMapView.onCreate(null);
             mMapView.onResume();
@@ -66,17 +85,101 @@ public class MapaTesoros extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
+/*        MapsInitializer.initialize(getContext());
 
         mGoogleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         googleMap.addMarker(new MarkerOptions().position(new LatLng(40.000223, -74)).title("Tu vieja").snippet("asd"));
 
-        CameraPosition Liberty= CameraPosition.builder().target(new LatLng(40.000223, -74)).zoom(16).bearing(0).tilt(45).build();
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.000223, -74)).zoom(16).bearing(0).tilt(45).build();
 
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));*/
+
+        mMap=googleMap;
+
+        miUbicacion();
     }
+
+    private void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 14);
+
+        if (marcador != null) {
+
+            marcador.remove();
+            circle.remove();
+
+
+        }
+
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Mi Posición Actual").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.animateCamera(miUbicacion);
+
+        circle = mMap.addCircle(new CircleOptions()
+                .center(coordenadas)
+                .radius(1000)
+                .strokeColor(0XFFe79e55)
+                .fillColor(0X26b2ffff));
+
+    }
+
+    private void actualizarUbicacion(Location location) {
+
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+
+        }
+
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getContext().getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locListener);
+
+
+    }
+
+
+
 
     /**
      * This interface must be implemented by activities that contain this
@@ -92,4 +195,7 @@ public class MapaTesoros extends Fragment implements OnMapReadyCallback {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+
 }
