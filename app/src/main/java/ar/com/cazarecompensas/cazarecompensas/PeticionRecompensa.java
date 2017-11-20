24a1814e84b70392b009c1cb59d163cc7415dab4
lauuteeh <lauuteeh@users.noreply.github.com>
2017-11-20 18:00:36 -0300

    PeticionRecompensa | Arreglo Texto y boton back

--- app/src/main/java/ar/com/cazarecompensas/cazarecompensas/PeticionRecompensa.java
+++ app/src/main/java/ar/com/cazarecompensas/cazarecompensas/PeticionRecompensa.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -237,6 +238,18 @@ public class PeticionRecompensa extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent = new Intent(this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
